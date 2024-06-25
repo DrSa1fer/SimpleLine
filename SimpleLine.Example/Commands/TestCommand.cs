dbc0c4a413294de8d550280aa30cf1d969d81ead
Danil Kucherenko <thekucherenko@gmail.com>
2024-06-25 18:58:41 +0300

    SL004A

--- SimpleLine.Example/Commands/TestCommand.cs
+++ SimpleLine.Example/Commands/TestCommand.cs
@@ -12,133 +12,13 @@ namespace SimpleLineLibrary.Example.Commands
         }
     }
 
-    [CommandDefinitions("test test")]
-    public class TestCommand1
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("test test test")]
+    [CommandDefinitions("test")]
     public class TestCommand2
     {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
+        [Command("@")]
+        public static void Foo(string message)
         {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions]
-    public class TestCommand3
-    {
-        [Command("sum")]
-        public static void Foo(int x, int y)
-        {
-            Console.WriteLine(x + y);
-        }
-    }
-
-    [CommandDefinitions("test4")]
-    public class TestCommand4
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("test5")]
-    public class TestCommand5
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("test6")]
-    public class TestCommand6
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("testq")]
-    public class TestCommandq
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("test1q")]
-    public class TestCommand1q
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("test2q")]
-    public class TestCommand2q
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("test3q")]
-    public class TestCommand3q
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("test4q")]
-    public class TestCommand4q
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("test5q")]
-    public class TestCommand5q
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
-        }
-    }
-
-    [CommandDefinitions("test6q")]
-    public class TestCommand6q
-    {
-        [Command("foo")]
-        public static void Foo(string[] msgs)
-        {
-            Console.WriteLine(string.Join(";", msgs));
+            Console.WriteLine("| " + message + " |");
         }
     }
 }
