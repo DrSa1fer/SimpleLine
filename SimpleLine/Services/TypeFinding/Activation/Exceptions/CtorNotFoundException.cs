e81edd16dc46716042fa36dd5213a35ef6b3a4f9
Danil Kucherenko <thekucherenko@gmail.com>
2024-06-29 12:00:52 +0300

    SL001B.4 :(

--- SimpleLine/Services/TypeFinding/Activation/Exceptions/CtorNotFoundException.cs
+++ SimpleLine/Services/TypeFinding/Activation/Exceptions/CtorNotFoundException.cs
@@ -1,6 +1,6 @@
 namespace SimpleLineLibrary.Services.TypeFinding.Activation.Exceptions
 {
-    public class CtorNotFoundException : Exception
+    internal class CtorNotFoundException : Exception
     {
         public CtorNotFoundException(Type type) 
             : base($"Type {type} doesnt contains constructor with avalible inject types")
