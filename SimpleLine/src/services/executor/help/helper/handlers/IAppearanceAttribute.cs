0adeb2232cb77a5496dccf6ccf71f64c6e502474
Danil Kucherenko <112440539+DrSa1fer@users.noreply.github.com>
2025-02-07 22:50:48 +0000

    Add IAppearanceAttribute

---
+++ SimpleLine/src/services/executor/help/helper/handlers/IAppearanceAttribute.cs
@@ -0,0 +1,7 @@
+namespace simpleline.services.executor.help.helper.handlers;
+
+internal interface IAppearanceAttribute {
+    public string Title { get; }
+    public string LeftBorder { get; }
+    public string RightBorder { get; }
+}
